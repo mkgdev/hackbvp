770f2c535199212679cd5b91ad5d9c2fb2321ea4
mkgdev <mukeshinhere07@gmail.com>
2017-10-10 19:32:25 +0530

    Object detection added

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ var express = require('express');
 var app     = express();
 var session = require('express-session');
 var indexRoute = require('./route/index.js');
+var watson = require('watson-developer-cloud');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var visual_recognition = watson.visual_recognition({
+  api_key: '36fe4dec42a5b39493c0768911d6550553843427',
+  version: 'v3',
+  version_date: '2016-05-20'
+});
+
+
+app.set('view engine','ejs');
+app.use(express.static(__dirname+'/public'));
 
 app.use(session({
   secret: 'keyboard cat',
@@ -9,6 +21,10 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: true }
 }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var nonBio = ['bottle','plastic','flask','thermal','bags', 'bag', 'water bottle', 'mobile', 'mobiles'];
+
 
 
 //**************************
@@ -18,6 +34,61 @@ app.use(session({
  app.use(indexRoute);
 
 //**************************
+
+//**********************************
+//         IBM WATSON
+//***************************
+var ans=[];
+
+var params = {
+url:"https://img.tradeindia.com/fp/0/209/empty-plastic-bottles-561.jpg"
+};
+
+visual_recognition.classify(params, function(err, res) {
+  if (err)
+    console.log(err);
+  else
+  {
+    var apiData = res;
+    console.log(JSON.stringify(res, null, 2));
+    apiData.images[0].classifiers[0].classes.forEach(function(data)
+  {
+   ans.push(data.class.split(" "));
+
+}
+  );
+  printAns();
+
+  }
+});
+
+function printAns()
+{
+  console.log(ans);
+}
+
+
+//*****************************
+
+//*******************************************
+//            Checking the API data
+//******************************************
+
+// apiData.images.classifiers.forEach(function(obj)
+// {
+//
+//   console.log(obj);
+// }
+
+
+
+// );
+
+
+
+//********************************************
+
+
 
 
 app.listen('8080', function()
